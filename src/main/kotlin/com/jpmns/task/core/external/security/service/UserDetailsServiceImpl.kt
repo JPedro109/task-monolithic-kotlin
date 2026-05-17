516e5194ca5be922f7223cda6ca8ffd310eef2d9
João Pedro <joaopedromoraesnsilva@gmail.com>
2026-05-17 00:29:23 -0300

    refactor: remove use of try catch

--- src/main/kotlin/com/jpmns/task/core/external/security/service/UserDetailsServiceImpl.kt
+++ src/main/kotlin/com/jpmns/task/core/external/security/service/UserDetailsServiceImpl.kt
@@ -3,26 +3,18 @@ package com.jpmns.task.core.external.security.service
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 
 import com.jpmns.task.core.application.usecase.user.dto.input.GetUserByIdInputDTO
-import com.jpmns.task.core.application.usecase.user.exception.UserNotFoundException
 import com.jpmns.task.core.application.usecase.user.interfaces.GetUserByIdUseCase
-import com.jpmns.task.core.domain.common.exception.DomainException
 
 @Service
 class UserDetailsServiceImpl(
     private val getUserByIdUseCase: GetUserByIdUseCase
 ) : UserDetailsService {
     override fun loadUserByUsername(username: String): UserDetails {
-        val userOutput = try {
-            getUserByIdUseCase.execute(GetUserByIdInputDTO(id = username))
-        } catch (ex: UserNotFoundException) {
-            throw UsernameNotFoundException("User not found: $username", ex)
-        } catch (ex: DomainException) {
-            throw UsernameNotFoundException("User not found: $username", ex)
-        }
+        val userOutput = getUserByIdUseCase.execute(GetUserByIdInputDTO(id = username))
+
         return User(userOutput.id, userOutput.password, emptyList())
     }
 }
